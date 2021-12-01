d634c9aeeb4ef495c475e2eaca5b366f6507577e
haphuong23399 <phamhaphuong23399@gmail.com>
2021-12-01 14:21:47 +0700

    bai 2 php

---
+++ Bai2-Form.php
@@ -0,0 +1,17 @@
+<html>
+<head><b>Bài 2: Bài tập với chuỗi</b></head>
+<body>
+<form action="Bai2-PHP.php" method="get">
+<p>1. Viết hàm đảo ngược 1 chuỗi</p>
+<p>2. Chuyển chuỗi đã cho thành chữ HOA</p>
+<p>3. Kiểm tra xem chuỗi đã cho có chứa một chuỗi nào đó không</p>
+<p>4. Đếm số ký tự xuất hiện trong chuỗi và hiển thị</p>
+<p>5. Thay thế chữ bất kỳ trong chuỗi thành chữ khác</p>
+Nhập chuỗi: <input type="text" name="txt" required><br><br>
+Nhập chuỗi kiểm tra (3): <input type="text" name="txtcheck" required><br><br>
+Nhập kí tự thay thế (5): Thay kí tự <input type="text" name="txta" required> 
+thành kí tự <input type="text"name="txtb" required><br><br>
+<input type="submit">
+</form>
+</body>
+</html>
